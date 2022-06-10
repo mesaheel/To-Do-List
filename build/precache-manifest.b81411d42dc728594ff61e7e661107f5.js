4d4ba30fa99f10e27613861ea0f2c071251481f4
Sahil Ali <sahil@Sahils-MacBook-Air.local>
2022-06-10 19:56:51 +0530

    first commit

---
+++ build/precache-manifest.b81411d42dc728594ff61e7e661107f5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d28afe13b02d7dca400d31a3abe75b4",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/To-do-list/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "79e6af6021dfccfbab46",
+    "url": "/To-do-list/static/js/main.f9384bcb.chunk.js"
+  },
+  {
+    "revision": "eef5bd35e101595273e0",
+    "url": "/To-do-list/static/js/runtime-main.5ebe4cab.js"
+  }
+]);
